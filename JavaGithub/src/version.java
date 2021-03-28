12c8c777d791b7492d29f532abae7f2e3ff9687a
Łukasz Jurek <lukasz.jurek42@o2.wp.pl>
2021-03-28 18:34:53 +0200

    Usuwam nazwe projektu, odczytuje autorow

--- JavaGithub/src/version.java
+++ JavaGithub/src/version.java
@@ -1,9 +1,12 @@
+import DEV1.java;
+import DEV2.java;
 
 public class version {
-	
-	System.out.println("Nazwa projektu = Version.java");
 	public static void main(String[] args) {
-		
+		String autor1=DEV1.author;
+		String autor2=DEV2.autor;
+		System.out.println(autor1);
+		System.out.println(autor2);
 		System.out.println("java version \"1.8.0_271\"");
 		System.out.println("Java(TM SE Runtime Environment (build 1.8.0_271-b09)");
 	}
