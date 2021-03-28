b5aecd5726f8939d628032d593e8945e19058e43
Michał Pieczka <michalpieczka99@gmail.com>
2021-03-28 18:08:31 +0200

    dodaje dev2.java

--- JavaGithub/src/version.java
+++ JavaGithub/src/version.java
@@ -5,6 +5,6 @@ public class version {
 	public static void main(String[] args) {
 		
 		System.out.println("java version \"1.8.0_271\"");
-		System.out.println("Java(TM) SE Runtime Environment (build 1.8.0_271-b09)");
+		System.out.println("Java(TM SE Runtime Environment (build 1.8.0_271-b09)");
 	}
 }
